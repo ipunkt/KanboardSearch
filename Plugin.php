f16a7588dce389b761f637056040556c7b119230
andrei <av@ipunkt.biz>
2019-07-09 10:38:38 +0200

    1st version. Search in comments, Task titles, subtask titles, description.

--- Plugin.php
+++ Plugin.php
@@ -2,14 +2,24 @@
 
 namespace Kanboard\Plugin\AdvancedFulltextSearch;
 
-
+use Kanboard\Core\Filter\LexerBuilder;
 use Kanboard\Core\Plugin\Base;
+use Kanboard\Plugin\AdvancedFulltextSearch\Filter\AdvancedSearchFilter;
 
 class Plugin extends Base
 {
     public function initialize()
     {
 
+        $this->container->extend('taskLexer', function($taskLexer, $c) {
+            /**
+             * @var LexerBuilder $taskLexer
+             */
+            $taskLexer->withFilter(AdvancedSearchFilter::getInstance()
+                ->setDatabase($c['db']), true);
+
+            return $taskLexer;
+        });
     }
 
     public function getPluginName()
