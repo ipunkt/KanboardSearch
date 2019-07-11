48ddf50834d4140c2a74d46e01350ecc05965399
andrei <av@ipunkt.biz>
2019-07-11 16:32:32 +0200

    WIP: click on ASF settings shows ASF content settings page

--- Template/config/sidebar.php
+++ Template/config/sidebar.php
@@ -1,3 +1,4 @@
 <li <?= $this->app->checkMenuSelection('AdvancedSearchController', 'asf') ?>>
-    <?= $this->url->link(t('Advanced Search Filter settings'), 'AdvancedSearchController', 'asf') ?>
+    <?= $this->url->link(t('Advanced Search Filter settings'), 'AdvancedSearchController', 'asf',
+        ['plugin' => 'AdvancedFulltextSearch']) ?>
 </li>
