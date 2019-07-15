1183ba5ea609e41330d149d329cae30189df08b0
andrei <av@ipunkt.biz>
2019-07-15 11:41:36 +0200

    German & Russian translations are implemented

---
+++ Locale/ru_RU/translations.php
@@ -0,0 +1,13 @@
+<?php
+return array(
+    'Advanced Search Filter' => 'Улучшенный поисковой фильтр',
+    'Advanced Search Filter settings' => 'Настройки улучшеного поискового фильтра',
+    'This plugin is used for advanced fulltext search within all Projects' => 'Этот плагин позволяет выполнять более детальный поиск во всех проэктах',
+    'Enable "Search in Comment"' => 'активировать "Поиск в комментариях" ',
+    'Enable "Search in Title"' => 'активировать "Поиск в заглавии" ',
+    'Enable "Search in Description"' => 'активировать "Поиск по описанию" ',
+    'Enable "Search in Subtask Title"' => 'активировать "Поиск в подзадачах" ',
+    'Enable "Search in Attachments"' => 'активировать "Поиск во вложениях" ',
+    'Save' => 'Сохранить',
+    'Settings' => 'Настройки',
+);
